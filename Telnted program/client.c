bea45f8cad7bbcc66566552aab5a2f6940208023
sivansalzmann <sivansalzmann10@gmail.com>
2021-01-02 23:55:10 -0800

    client side start

---
+++ Telnted program/client.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+int sock;
+char close_string[] = {'c','l','o','s','e',' ','c','l','i','e','n','t','\0'};
+/*
+ * Interupt handler
+ */
+
+void sigHandle(int sig) {
+  printf("\nInterrupt- closing client socket\n");
+  close(sock);
+  exit(-1);
+}
+
+/*
+ * MAIN()
+ */
+int main(int argc, char **argv) {
+  signal(SIGINT, sigHandle);
+  int port;
+
+  if (argc <= 1) {
+    printf("needs to provide port numer\n");
+    exit(-1);
+  }
+}
