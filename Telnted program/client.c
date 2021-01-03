157408de3ce64c34c5f82e46bdb0e6715d0e8773
sivansalzmann <sivansalzmann10@gmail.com>
2021-01-03 02:55:20 -0800

    adding method send message to the server

--- Telnted program/client.c
+++ Telnted program/client.c
@@ -19,6 +19,40 @@ struct sockaddr_in server;
 struct sockaddr_in client;
 
 /*
+ * This method is sending message to the server
+ */
+void messgServer() {
+  char buffer[MAX];
+  int n;
+  for (;;) {
+    bzero(buffer, sizeof(buffer));
+    printf("Enter the string : ");
+    n = 0;
+    while ((buffer[n++] = getchar()) != '\n')
+      ;
+  
+   buffer[n-1] = '\0';
+    if (write(sock, buffer, sizeof(buffer)) < 0) 
+    {
+    	printf("No response from server, closing client socket");
+    	close(sock);
+     	exit(-1);
+    }
+
+    if(strcmp(close_string, buffer) == 0)
+    {
+	printf("connection closed, bye bye\n");
+	close(sock);
+	exit(0);
+    }
+    bzero(buffer, sizeof(buffer));
+    read(sock, buffer, sizeof(buffer));
+    printf("From Server :\n%s\n", buffer);
+  }
+}
+
+
+/*
  * Interupt handler
  */
 
