038dfd0841d055d51e50cf099526994185b3a0da
sivansalzmann <sivansalzmann10@gmail.com>
2021-01-03 01:03:01 -0800

    add answer from server to server.c

--- Telnted program/server.c
+++ Telnted program/server.c
@@ -4,14 +4,46 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <unistd.h>
-
+#include <signal.h>
 
 #define netErr -1
+#define MaxData 150
 
 int sock;
 int client_socks[5] = {0};
 int clients_size = 0;
 char close_string[] = {'c','l','o','s','e',' ','c','l','i','e','n','t','\0'};
+
+/*
+ * This method handling the string that sends over the connection
+ */
+void stringHandler(fd_set *readfds) {
+  int data_len;
+  char data[MaxData];
+  for (int i = 0; i < clients_size; i++)
+ {
+    int client_sock = client_socks[i];
+    if (FD_ISSET(client_sock, readfds)) {
+      data_len = recv(client_sock, data, MaxData, 0);
+
+      if (data_len == 0) 
+	//closeConnection(client_sock, i);
+
+
+      if (data_len) {
+	data[data_len-2] = '\0';
+  	if(strcmp(close_string, data) == 0)
+  	{
+		printf("comprasion worked\n");
+		//closeConnection(client_sock, i);    		
+	}
+	else
+		printf("%s","here1");
+		//runExec(client_sock, data, data_len);
+      }
+    }
+  }
+}
 
 /*
  * This method is creating the listner socket
@@ -47,6 +79,31 @@ int listnerSocket(char *p) {
 }
 
 /*
+ * Interupt handler
+ */
+
+void signalHandler(int sig) 
+{
+
+  int closed_connections=0;
+
+  for (int i=0; i<clients_size; i++)
+  {
+    if(client_socks[i]!=0)
+    {
+      closed_connections++;
+      close(client_socks[i]);     
+     }    
+  }
+
+  printf("number of client closed - %d \n",closed_connections);
+  close(sock);
+  printf("closed server\n");
+  exit(-1);
+}
+
+
+/*
  * MAIN()
  */
 int main(int argc, char *argv[]) 
@@ -59,13 +116,19 @@ int main(int argc, char *argv[])
   }
 
   fd_set readfds;
+
+  signal(SIGABRT,signalHandler);
   
   char * port = argv[1];
   sock = listnerSocket(port);
 
   if (sock != netErr) 
  {
-	printf("%d",1);
+	while (1) 
+	{
+		printf("%s","here");
+		stringHandler(&readfds);
+	}
   }
   close(sock);
 }
